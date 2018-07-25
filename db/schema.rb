40b09f7cef9b0c5d31b022ba112e34f11991d0a8
Léona Chevrel <chevrel.leona@gmail.com>
2018-07-25 17:47:40 +0200

    solved the problems

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2018_07_25_150124) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
-    t.string "decription"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
