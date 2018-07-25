ae33c7e69d13e1386e03bfaa82b30b73e1e2a0c9
christopheleray <christophe.leray1@gmail.com>
2018-07-25 17:32:40 +0200

    ajout des model student, course, lesson

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do 
+  student =  Student.create(first_name: Faker::Name.name, last_name: Faker::Name.name, email: Faker::Internet.email, course_id: Faker::Number.between(1, 10))
+end
+
+10.times do
+  course = Courses.create(title: Faker::FunnyName.name, description: Faker::Lorem.paragraph)
+end
+
+10.times do
+  lesson = Lesson.create(title: Faker::Beer.name, content: Faker::Beer.style, course_id: Faker::Number.between(1, 10))
+ end 
